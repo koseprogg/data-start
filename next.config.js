d09225a2656f11c04dffdd2dab31271aa073adee
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2022-01-17 00:01:14 +0100

    fix ci

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
+const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   reactStrictMode: true,
   /**
    * Tell Next.js where the `public` folder is.
    * Replace `nextjs-github-pages` with your Github repo project name.
    */
-  assetPrefix: isProd ? "/nextjs-github-pages/" : "",
+  assetPrefix: isProd ? "/data-start/" : "",
 };
