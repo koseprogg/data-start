72c0c8ebe548a15f8dab7c7a72c903d5ce6fe299
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2022-01-16 23:59:44 +0100

    fix ci

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   reactStrictMode: true,
-}
+  /**
+   * Tell Next.js where the `public` folder is.
+   * Replace `nextjs-github-pages` with your Github repo project name.
+   */
+  assetPrefix: isProd ? "/nextjs-github-pages/" : "",
+};
