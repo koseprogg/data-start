3ff687b3646b9f7cf2c906e22aba20608738b5fb
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2022-01-18 12:16:55 +0100

    New test

--- components/Image.js
+++ components/Image.js
@@ -1,12 +1,10 @@
 import React from "react";
 import NextImage from "next/image";
 
-const basePath = process.env.NODE_ENV === "production" ? "/data-start" : "";
-
 const Image = (props) => {
   return (
     <>
-      <NextImage src={`${basePath}${props.src}`} {...props} />
+      <NextImage src={`${props.src.src.trim("/data-start")}`} {...props} />
     </>
   );
 };
