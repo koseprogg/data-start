44e2a5befa6165048801ed5c141c52e54101984a
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2022-02-09 18:46:15 +0100

    Sketch api for joblistings

--- pages/testAPI.js
+++ pages/testAPI.js
@@ -1,14 +1,28 @@
 import React, { useEffect, useState } from "react";
 
-const testAPI = () => {
+const api = {
+  deadline: "",
+  fromYear: 1,
+  toYear: 5,
+  jobType: "",
+  title: "",
+  companyName: "",
+  url: "",
+  workplace: "",
+  logo: "", // Kanskje?
+};
+
+const TestAPI = () => {
   const [Jobs, setJobs] = useState({});
   useEffect(() => {
-    fetch("https://lego.abakus.no/api/v1/joblistings/").then(async (response) => {
-      const result = await response.json()
-      setJobs(result);
-    });
+    fetch("https://lego.abakus.no/api/v1/joblistings/").then(
+      async (response) => {
+        const result = await response.json();
+        setJobs(result);
+      }
+    );
   }, []);
   return <div>{JSON.stringify(Jobs)}</div>;
 };
 
-export default testAPI;
+export default TestAPI;
