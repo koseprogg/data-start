11f9d7cba50ed90066c779138453b0fce7cbd315
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2022-02-09 18:05:38 +0100

    Setup firebase

--- components/Image.js
+++ components/Image.js
@@ -1,12 +1,11 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
-const basePath = process.env.NODE_ENV === "production" ? "/data-start/" : "";
 
 const Image = (props) => {
   return (
     <>
-      <img {...props} src={`${basePath}${props.src}`} />
+      <img {...props} src={`${props.src}`} />
     </>
   );
 };
