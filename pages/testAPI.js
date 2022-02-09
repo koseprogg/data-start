771bacaa01978ea84df239a48677c8feb0ca640c
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2022-02-09 19:51:39 +0100

    Add parser for Online jobs

--- pages/testAPI.js
+++ pages/testAPI.js
@@ -1,17 +1,5 @@
 import React, { useEffect, useState } from "react";
 
-const api = {
-  deadline: "",
-  fromYear: 1,
-  toYear: 5,
-  jobType: "",
-  title: "",
-  companyName: "",
-  url: "",
-  workplace: "",
-  logo: "", // Kanskje?
-};
-
 const TestAPI = () => {
   async function fetchAndParse(url, callback) {
     return fetch(url).then(async (response) => {
@@ -20,7 +8,19 @@ const TestAPI = () => {
     });
   }
 
-  function parseOnlineJobs(payload) {}
+  function parseOnlineJobs(payload) {
+    const result = payload.results;
+    return result.map((listing) => ({
+      deadline: listing.deadline ?? "Udefinert",
+      fromYear: 0,
+      toYear: 0,
+      jobType: listing.employment.name,
+      title: listing.title,
+      companyName: listing.company.name,
+      url: `https://online.ntnu.no/career/${listing.id}`,
+      workplace: listing.location.map((location) => location.name).toString(),
+    }));
+  }
 
   function parseAbakusJobs(payload) {
     return payload.results.map((listing) => ({
@@ -31,7 +31,9 @@ const TestAPI = () => {
       title: listing.title,
       companyName: listing.company.name,
       url: "https://abakus.no/joblistings/" + listing.id + "/",
-      workplace: listing.workplaces.map((workplace) => workplace.town).toString(),
+      workplace: listing.workplaces
+        .map((workplace) => workplace.town)
+        .toString(),
     }));
   }
   function parseTihldeJobs(payload) {
@@ -49,12 +51,21 @@ const TestAPI = () => {
   }
   const [Jobs, setJobs] = useState([]);
   useEffect(() => {
-    setJobs([]);
-    fetchAndParse("https://lego.abakus.no/api/v1/joblistings/", parseAbakusJobs).then((listings) => {
-      setJobs([...Jobs, ...listings]);
-    });
-    fetchAndParse("https://api.tihlde.org/jobposts/", parseTihldeJobs).then((listings) => {
-      setJobs([...Jobs, ...listings]);
+    const fetchAbakus = fetchAndParse(
+      "https://lego.abakus.no/api/v1/joblistings/",
+      parseAbakusJobs
+    );
+    const fetchOnline = fetchAndParse(
+      "https://old.online.ntnu.no/api/v1/career/",
+      parseOnlineJobs
+    );
+    const fetchTihlde = fetchAndParse(
+      "https://api.tihlde.org/jobposts/",
+      parseTihldeJobs
+    );
+
+    Promise.all([fetchAbakus, fetchOnline, fetchTihlde]).then((arrays) => {
+      setJobs(arrays.flat());
     });
   }, []);
   return <div>{JSON.stringify(Jobs)}</div>;
